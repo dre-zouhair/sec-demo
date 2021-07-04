5badfc0cf1dc70c28b3f0773db093d3ef029d9d5
dre-zouhair <dre.zouhair@gmail.com>
2021-07-04 15:18:39 +0200

    setting MyUserDetailsService and adding a fake user

--- src/main/java/com/dre/secdemo/secConfig/SecurityConfigurer.java
+++ src/main/java/com/dre/secdemo/secConfig/SecurityConfigurer.java
@@ -1,4 +1,31 @@
 package com.dre.secdemo.secConfig;
 
-public class SecurityConfigurer {
+import com.dre.secdemo.services.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public SecurityConfigurer(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Override
+    protected void  configure(AuthenticationManagerBuilder authenticationManagerBuilder)throws Exception{
+        authenticationManagerBuilder.userDetailsService(myUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
